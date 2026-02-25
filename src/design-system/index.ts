85085c5b2cc4ba17d27c79ffedd85f64093c43fa
fenilmodi00 <fenilmodi00@gmail.com>
2026-02-25 14:20:57 +0530

    docs: update design system documentation

--- src/design-system/index.ts
+++ src/design-system/index.ts
@@ -1,3 +1,14 @@
+/**
+ * Design System - MINT Design Tokens
+ * 
+ * This package provides design tokens from the MINT design system
+ * (https://github.com/Groww-OSS/webster/tree/develop/packages/mint-css)
+ * 
+ * Usage:
+ * import { colors, typography, spacing } from '@/design-system/tokens';
+ * import { mintColors, mintTypography, mintSpacing } from '@/design-system/tokens';
+ */
+
 export * from './tokens';
-export * from './components';
 export * from './themes';
+// export * from './components'; // Future: when we add component presets
